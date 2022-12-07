2fb2eefb296e9b7a83cfef34b0aa807abcacd4c1
baotran13 <bao.tran@techupcorp.com>
2022-12-07 10:31:26 +0700

    chorce<database>: change link database

--- src/v1/databases/init.mongodb.js
+++ src/v1/databases/init.mongodb.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 //connect mongoose
 mongoose.connect(
-  process.env.MONGO_URI,
- //`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.81ywfpz.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+  //process.env.MONGO_URI,
+ `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.81ywfpz.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
    { 
     useNewUrlParser: true, 
     autoIndex: true
--- src/v1/databases/init.redis.js
+++ src/v1/databases/init.redis.js
@@ -1,11 +1,11 @@
 const Redis = require('ioredis');
-const redis = new Redis(process.env.REDIS_URL); //local
-// const redis = new Redis({
-//     port:process.env.REDIS_PORT,
-//     host:process.env.REDIS_HOST,
-//     username:process.env.REDIS_USERNAME,
-//     password:process.env.REDIS_PASSWORD
-// })
+//const redis = new Redis(process.env.REDIS_URL); //local
+const redis = new Redis({
+    port:process.env.REDIS_PORT,
+    host:process.env.REDIS_HOST,
+    username:process.env.REDIS_USERNAME,
+    password:process.env.REDIS_PASSWORD
+})
 
 redis.ping(function (err, result) {
     console.log(result);
