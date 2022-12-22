ff49421ca42e56ab57ddafaf4ea4b93f027de1e1
baotran13 <bao.tran@techupcorp.com>
2022-12-23 05:04:56 +0700

    chorce<node>: set version node for render.com

--- src/v1/utils/apiFeatures.js
+++ src/v1/utils/apiFeatures.js
@@ -60,7 +60,7 @@ class APIFeatures {
       const queryCopy = { ...this.queryStr };
 
       // Removing fields from the query
-      const removeFields = ["limit", "curentPage", "sellerId", "categoryId", "name", "summary", "slug"];
+      const removeFields = ["limit", "currentPage", "sellerId", "categoryId", "name", "summary", "slug"];
       removeFields.forEach((el) => delete queryCopy[el]);
 
       // Advance filter for price, ratings etc
